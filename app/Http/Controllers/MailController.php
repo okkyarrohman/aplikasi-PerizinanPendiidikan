b9cc1c6792c592f12e30a2e6f6717d46b20707b1
okkyarrohman <okkyzee@gmail.com>
2023-10-29 01:06:15 +0700

    commit

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -10,28 +10,26 @@ use Dompdf\Dompdf;
 
 class MailController extends Controller
 {
-    public function send_attach_gmail($id)
+    public function send_attach_gmail($id, Request $request)
     {
         $data = array('name' => 'jarwo');
             $permohonans = PerizinanPendirian::where(['id' =>$id])->get();
+
             $dompdf = new Dompdf();
             $view = view('walikota.perizinanPendirian.tracking.izinTerbit.izinTerbitPdf',compact('permohonans'));
             $dompdf->loadHTML($view);
             $dompdf->render();
 
 
-        Mail::send(['file' => 'mail'], $data, function ($message)use($dompdf) {
 
-            $email = 'okkyzee@gmail.com';
-
-            $message->to($email)->subject('Surat Izin Terbit');
+        Mail::send(['file' => 'mail'], $data, function ($message)use($dompdf,$permohonans) {
+            $message->to('amarulj22@gmail.com')->subject('Surat Izin Terbit');
 
             $message->attachData($dompdf->output(),'surat_izin_terbit.pdf');
-
 
             $message->from('eightech@company.com','EighTech');
         });
 
-        echo "data has sent";
+        return back()->with('success','Data Berhasil Dikirim ke Email');
     }
 }
