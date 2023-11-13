567134959777a16eb2227aacb53e6ead3db1f972
okkyarrohman <okkyzee@gmail.com>
2023-11-13 10:05:54 +0700

    landing pages

--- resources/views/welcome2.blade.php
+++ resources/views/welcome2.blade.php
@@ -54,20 +54,18 @@
         </div>
     </div>
 
-    <div class="frame-1">
-        <div class="div">
-            <div class="text-wrapper">TECHNOPOLIS</div>
-            <p class="p">
-                Aplikasi layanan perizinan dibidang pendidikan berbasis mobile dan website yang bertujuan untuk membantu
-                para
-                masyarakat di Kota Surabaya untuk melakukan permohonan perizinan dibidang pendidikan secara online, bisa
-                di
-                mana saja dan kapan saja.
-            </p>
+    <div class="content-1">
+        <div class="text">
+
+            <h3>TECNOPOLIS</h3>
+            <p>Aplikasi layanan perizinan dibidang pendidikan berbasis mobile dan website yang bertujuan untuk membantu
+                para masyarakat di Kota Surabaya untuk melakukan permohonan perizinan dibidang pendidikan secara online,
+                bisa di mana saja dan kapan saja.</p>
+            <button class="btn btn-warning">Daftar Sekarang</button>
         </div>
-        <button class="button">
-            <div class="btn btn-warning">Daftar Sekarang</div>
-        </button>
+        <div class="gambar">
+            <img src="{{ asset('images/logo.png') }}" alt="" width="200" height="200">
+        </div>
     </div>
 
     <br>
@@ -103,6 +101,66 @@
     </div>
 
 
+    <div class="frame-3">
+        <div class="overlap-group">
+            <img class="group" src="img/group.png" />
+            <div class="frame-wrapper">
+                <div class="div-wrapper">
+                    <p class="text-wrapper">©Technopolis 2023. Hak Cipta Dilindungi Oleh Undang-undang</p>
+                </div>
+            </div>
+            <div class="div">
+                <div class="div-2">
+                    <img class="img" src="img/frame.svg" />
+                    <div class="div-3">
+                        <div class="div-4">
+                            <div class="text-wrapper-2">Pelajari</div>
+                        </div>
+                        <div class="div-wrapper">
+                            <div class="text-wrapper-3">Tentang</div>
+                        </div>
+                        <div class="div-wrapper">
+                            <div class="text-wrapper-3">Berita</div>
+                        </div>
+                    </div>
+                    <div class="div-5">
+                        <div class="div-wrapper-2">
+                            <div class="text-wrapper-4">Ikuti Kami</div>
+                        </div>
+                        <div class="div-4">
+                            <img class="image" src="img/image-23.png" />
+                            <div class="text-wrapper-5">technopolis</div>
+                        </div>
+                        <div class="div-4">
+                            <img class="group-2" src="img/group-173.png" />
+                            <div class="text-wrapper-5">technopolis</div>
+                        </div>
+                        <div class="div-4">
+                            <img class="image" src="img/image-24.png" />
+                            <div class="text-wrapper-5">technopolis</div>
+                        </div>
+                        <div class="div-4">
+                            <img class="group-2" src="img/image.png" />
+                            <div class="text-wrapper-5">technopolis</div>
+                        </div>
+                    </div>
+                    <div class="div-6">
+                        <div class="div-4">
+                            <div class="text-wrapper-2">Hubungi Kami</div>
+                        </div>
+                        <div class="div-7">
+                            <div class="iconly-bulk-message"></div>
+                            <div class="text-wrapper-5">technopolisgmail.com</div>
+                        </div>
+                        <div class="div-7">
+                            <div class="iconly-bulk-call"></div>
+                            <div class="text-wrapper-5">082277770000</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <script src="{{ asset('dashboard/js/bootstrap.js') }}"></script>
     <script src="{{ asset('dashboard/js/app.js') }}"></script>
